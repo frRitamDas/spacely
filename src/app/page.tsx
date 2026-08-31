be53ecd4f8b37d7455cf1fdd0282f4c40a8c19c0
frRitamDas <ritamdas20012@gmail.com>
2026-09-01 00:09:41 +0530

    feat: compose personalized rails into home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,12 @@ import useSpacelySettings from "@/hooks/useSpacelySettings";
 const Hero = dynamic(() => import("@/components/sections/Home/Hero"));
 const ContinueWatching = dynamic(() => import("@/components/sections/Home/ContinueWatching"));
 const HomePageList = dynamic(() => import("@/components/sections/Home/List"));
+const PersonalizedRails = dynamic(() => import("@/components/sections/Home/PersonalizedRails"));
 const InfiniteCategories = dynamic(() => import("@/components/sections/Home/InfiniteCategories"));
 
 const HomePage: NextPage = () => {
   const settings = useSpacelySettings();
-  return (
-    <div className="flex flex-col gap-10 pb-16 md:gap-16">
-      <Hero />
-      <div className="flex flex-col gap-12 md:gap-16">
-        {settings.continueWatching && !settings.pauseWatchHistory && <ContinueWatching />}
-        <HomePageList />
-        <InfiniteCategories />
-      </div>
-    </div>
-  );
+  return <div className="flex flex-col gap-10 pb-16 md:gap-16"><Hero /><div className="flex flex-col gap-12 md:gap-16">{settings.continueWatching && !settings.pauseWatchHistory && <ContinueWatching />}<PersonalizedRails /><HomePageList /><InfiniteCategories /></div></div>;
 };
 
 export default HomePage;
