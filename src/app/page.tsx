46c5a94a78c0028fc050d0dd24e925b7b871bca9
frRitamDas <ritamdas20012@gmail.com>
2026-09-01 00:06:31 +0530

    feat: wire premium preferences and infinite home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
+"use client";
+
 import { NextPage } from "next";
 import dynamic from "next/dynamic";
+import useSpacelySettings from "@/hooks/useSpacelySettings";
 
 const Hero = dynamic(() => import("@/components/sections/Home/Hero"));
 const ContinueWatching = dynamic(() => import("@/components/sections/Home/ContinueWatching"));
 const HomePageList = dynamic(() => import("@/components/sections/Home/List"));
+const InfiniteCategories = dynamic(() => import("@/components/sections/Home/InfiniteCategories"));
 
 const HomePage: NextPage = () => {
+  const settings = useSpacelySettings();
   return (
     <div className="flex flex-col gap-10 pb-16 md:gap-16">
       <Hero />
       <div className="flex flex-col gap-12 md:gap-16">
-        <ContinueWatching />
+        {settings.continueWatching && !settings.pauseWatchHistory && <ContinueWatching />}
         <HomePageList />
+        <InfiniteCategories />
       </div>
     </div>
   );
