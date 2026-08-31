6f694d9d8c58ef07ec832cd0bdb269b4f02ec3e6
frRitamDas <ritamdas20012@gmail.com>
2026-08-31 18:05:27 +0530

    fix: remove incompatible Next TypeScript ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,5 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
 
 export default defineConfig([
-  ...nextVitals,
   globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
-  {
-    rules: {
-      "@typescript-eslint/no-explicit-any": "warn",
-      "@typescript-eslint/no-unused-vars": "warn",
-      "@typescript-eslint/no-unused-expressions": "off",
-      "react/no-unescaped-entities": "warn",
-    },
-  },
 ]);
