0eb87c8183d399372472cb23158314a1f2a4c0e1
frRitamDas <ritamdas20012@gmail.com>
2026-08-31 22:51:11 +0530

    feat(ui): place cinematic hero on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import { NextPage } from "next";
 import dynamic from "next/dynamic";
+
+const Hero = dynamic(() => import("@/components/sections/Home/Hero"));
 const ContinueWatching = dynamic(() => import("@/components/sections/Home/ContinueWatching"));
 const HomePageList = dynamic(() => import("@/components/sections/Home/List"));
 
 const HomePage: NextPage = () => {
   return (
-    <div className="flex flex-col gap-3 md:gap-8">
+    <div className="flex flex-col gap-8 pb-12 md:gap-12">
+      <Hero />
       <ContinueWatching />
       <HomePageList />
     </div>
